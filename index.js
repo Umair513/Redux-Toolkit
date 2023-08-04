003ece7a7ec626ab94a563e21eb33087be9692d1
Umair513 <soomroumair513@gmail.com>
2023-08-04 16:44:48 +0500

    reducers

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 const CAKE_ORDERED = "CAKE_ORDERED"
 
 function orderCake() {
@@ -5,4 +7,21 @@ function orderCake() {
         type: CAKE_ORDERED,
         quantity: 1
     }
+}
+
+const initialState = {
+    numOfCakes: 10
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case CAKE_ORDERED:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes -1
+            }
+        default:
+            return state
+    }
+
 }
